41519e936e3e79dfaa37e0e0d2c439b7df4b7613
Francisco Dias <frandias@gmail.com>
2016-01-26 19:37:59 -0500

    have a grid

--- sketch.js
+++ sketch.js
@@ -3,7 +3,12 @@ function setup () {
 }
 
 function draw() {
-  
+  background(255) // White
+  for (var x = 0; x < width; x += 20) {
+    for (var y = 0; y < height; y +=20) {
+      ballStick(x, y, 5);
+    }
+  }
 }
 
 var ballStick = function(x,y, ellipseSize) {
