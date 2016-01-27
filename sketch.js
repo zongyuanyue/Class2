35f97ea2161eb25f537fda581860f4de5f5cf55b
Francisco Dias <frandias@gmail.com>
2016-01-26 20:02:56 -0500

    bubbles are scared

--- sketch.js
+++ sketch.js
@@ -6,12 +6,16 @@ function draw() {
   background(255) // White
   for (var x = 0; x < width; x += 20) {
     for (var y = 0; y < height; y +=20) {
-      ballStick(x, y, 5);
+      new BallStick(x,y, howFar(x,y))
     }
   }
 }
 
-var ballStick = function(x,y, ellipseSize) {
-  line(x, y, mouseX, mouseY)
+var BallStick = function(x,y, ellipseSize) {
+  // line(x, y, mouseX, mouseY)
   ellipse(x, y, ellipseSize, ellipseSize)
+}
+
+function howFar(p1,p2) {
+  return dist(p1, p2, mouseX, mouseY) / (width /40) ;
 }
