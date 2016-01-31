4fbe9aa99bcd9dfb9649288674887929149ee867
Francisco Dias <frandias@gmail.com>
2016-01-31 17:05:08 -0500

    make the eyes move

--- sketch.js
+++ sketch.js
@@ -6,8 +6,6 @@ function setup() {
 
 function draw() {
   background('green');
-  
-  
   eyeballs.display();
   eyeballs.pupils();
 }
@@ -15,54 +13,43 @@ function draw() {
 
 //Constructor
 function Eyeballs(x,y) {
-  
-  //pupil movement:
-  var pupilX = 150;
-  var pupilY = 250;
-  var scleraX = 150;
-  var scleraY = 250;
-  
+
+  //this.pupil movement:
+  scleraX = 150;
+  scleraY = 250;
+  scleraSize = 100
+  pupilX = 150;
+  pupilY = 250;
+  pupilSize = scleraSize / 2;
+
+  //white parts of eyes
   this.display = function() {
-    
-    //white parts of eyes 
     stroke(255);
     fill('white');
-    ellipse(scleraX,scleraY,100,100);
-    ellipse(scleraX + 200,scleraY,100,100);
+    ellipse(scleraX,scleraY,scleraSize,scleraSize);
+    ellipse(scleraX + 200,scleraY,scleraSize,scleraSize);
   }
-  
+
   this.pupils = function() {
-    
-    //black parts of eyes 
+    var currentDist = dist(pupilX,pupilY,scleraX,scleraY);
+    //black parts of eyes
     stroke(0);
     fill('black');
-    ellipse(pupilX,pupilY,50,50);
-    ellipse(pupilX + 200,pupilY,50,50);
-    
-    //how far can the pupils move?
-    function dist(pupilX,pupilY,scleraX,scleraY) {
-      
-      //ellipse(scleraX,scleraY,100,100);
-      //ellipse(scleraX + 200,scleraY,100,100);
-      
-      if(mouseX > pupilX && pupilX < scleraX) {
+    ellipse(pupilX,pupilY,pupilSize,pupilSize);
+    ellipse(pupilX + 200,pupilY,pupilSize,pupilSize);
+
+      if(mouseX > pupilX && pupilX < scleraX + pupilSize / 2) {
         pupilX = pupilX + 1;
       }
-      else if(mouseX < pupilX && pupilX < scleraX) {
+      if(mouseX < pupilX && pupilX > scleraX - pupilSize / 2) {
         pupilX = pupilX - 1;
       }
-      else{
-        pupilX;
-      }
-      if(mouseY > pupilY && pupilY < scleraY) {
+      if(mouseY > pupilY && pupilY < scleraY + pupilSize / 2) {
         pupilY = pupilY + 1;
       }
-      else if(mouseY < pupilY && pupilY < scleraY){
+      if(mouseY < pupilY && pupilY > scleraY - pupilSize / 2){
         pupilY = pupilY - 1;
       }
-      else{
-        pupilY;
-      }
-    }
   }
+
 }
